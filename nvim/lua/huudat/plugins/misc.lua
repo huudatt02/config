586d2ff8f899a60060facfa949b6ee73c360cd21
huudat <huudatnguyen1912@gmail.com>
2025-10-14 22:51:04 +0700

    add misc neovim config

---
+++ nvim/lua/huudat/plugins/misc.lua
@@ -0,0 +1,60 @@
+return {
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {},
+	},
+	{
+		"ojroques/nvim-bufdel",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>x", "<cmd>BufDel<CR>", desc = "Close buffer" },
+			{ "<leader>X", "<cmd>BufDel!<CR>", desc = "Force close buffer" },
+			{ "<leader>ba", "<cmd>BufDelAll<CR>", desc = "Close all buffers" },
+			{ "<leader>bo", "<cmd>BufDelOthers<CR>", desc = "Close other buffers" },
+		},
+		opts = {
+			next = "tabs",
+			quit = false,
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"kylechui/nvim-surround",
+		event = "VeryLazy",
+		version = "*",
+		opts = {},
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		main = "ibl",
+		opts = {
+			indent = { char = "│" },
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"help",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"lazy",
+				},
+				buftypes = { "terminal", "nofile" },
+			},
+		},
+	},
+}
